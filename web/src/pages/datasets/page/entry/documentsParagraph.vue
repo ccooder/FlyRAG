9a324681dbf51973f2b87812bf35e349af9ee642
wufeng <763476339@qq.com>
2025-07-23 17:53:05 +0800

    feat： 分段列表

--- web/src/pages/datasets/page/entry/documentsParagraph.vue
+++ web/src/pages/datasets/page/entry/documentsParagraph.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: WuFeng <763467339@qq.com>
  * @Date: 2025-07-23 17:30:17
- * @LastEditTime: 2025-07-23 17:44:27
+ * @LastEditTime: 2025-07-23 17:50:51
  * @LastEditors: WuFeng <763467339@qq.com>
  * @Description: 
  * @FilePath: \FlyRAG\web\src\pages\datasets\page\entry\documentsParagraph.vue
